9fe075a65927d55530e8353656bd01979e41875d
ElHakikAmina <amina.el.hakik2@gmail.com>
2023-03-16 17:12:30 +0100

    vueJs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="wrapper">
+  <h1> Lise to do</h1>
+  <div>
+    <form class="inputField" @submit.prevent="create">
+      <input v-model="newTodo" autocomplete="off">
+      <button>ajout</button>
+    </form>
+  </div>
+  <ul class="todoList">
+    <li v-for="item in todos" :key="item.id">
+      <h3 :class="{ done : item.completed}" @click="completed(item)">{{ item.content }}</h3>
+      <button @click="remove(item)">supprimer</button>
+    </li>
+  </ul>
+  <div class="footer">
+    <button @click="markAll()">selectionner tous</button>
+  </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const todos = ref([])
+    const newTodo = ref ('')
+    function create (){
+      todos.value.push({
+        id:Date.now(),
+        content:newTodo.value,
+        completed:false
+      })
+      newTodo.value =''
+      
+    }
+    function remove(item){
+        todos.value.splice(todos.value.indexOf(item),1)
+      }
+    function completed(item){
+      item.completed = !item.completed
+    }
+    function markAll()
+    {
+      todos.value.forEach((todo) => todo.completed = true)
+    }
+    return {
+      todos,
+      newTodo,
+      create,
+      remove,
+      completed,
+      markAll
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done{
+  text-decoration:line-through;
 }
 </style>
